c668f63582db08f7c7b5f373e264b9d17912cba4
Juan7334wqa <xue7334xi@gmail.com>
2025-10-29 15:45:41 +0100

    prue2

--- index.ts
+++ index.ts
@@ -1,89 +1,148 @@
+import express from 'express';
 import axios from 'axios';
-import express from "express";
-import cors from "cors";
-
-
+import cors from 'cors';
+ 
+ 
+ 
 type LD = {
-  id: number;
-  filmName: string;
-  rotationType: "CAV" | "CLV";
-  region: string;
-  lengthMinutes: number;
-  videoFormat: "NTSC" | "PAL";
-};
+id: number
+filmName: string 
+rotationType: "CAV" | "CLV",
+region: string,
+lengthMinutes: number,
+videoFormat: "NTSC" | "PAL"
+}
 
+let LDs:LD[]=[
+   { id: 1, filmName: "Lakers", rotationType: "CAV", region: "",lengthMinutes: 12, videoFormat:"NTSC"},
 
-let LDs: LD[] = [
-  { id: 1, filmName: "Lakers", rotationType: "CAV", region: "USA", lengthMinutes: 12, videoFormat: "NTSC" },
-  { id: 2, filmName: "Celtics", rotationType: "CLV", region: "EU", lengthMinutes: 24, videoFormat: "PAL" },
-];
+ { id: 2, filmName: "Celtics", rotationType: "CLV", region: "",lengthMinutes: 24, videoFormat:"PAL" },
 
+]
 const validateLD = (data: any): string | null => {
   if (!data) return "No se ha proporcionado ningún cuerpo de solicitud.";
+
   const { filmName, rotationType, region, lengthMinutes, videoFormat } = data;
 
   if (typeof filmName !== "string" || filmName.trim().length < 2)
     return "El nombre debe ser una cadena con al menos 2 caracteres.";
+
   if (typeof rotationType !== "string" || (rotationType !== "CLV" && rotationType !== "CAV"))
     return "El rotationType debe ser CLV o CAV.";
+
   if (typeof region !== "string" || region.trim().length < 2)
     return "El region debe ser una cadena con al menos 2 caracteres.";
+
   if (typeof lengthMinutes !== "number")
     return "El lengthMinutes debe ser un número.";
+
   if (typeof videoFormat !== "string" || (videoFormat !== "NTSC" && videoFormat !== "PAL"))
     return "El videoFormat debe ser NTSC o PAL.";
 
+  // Si todo está bien, no hay errores
   return null;
 };
 
 
+
+
 const app = express();
-app.use(cors());
-app.use(express.json());
+
+const port = 3000;
 
 
-app.get("/ld", (req, res) => res.json(LDs));
 
 
-app.get("/ld/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const ld = LDs.find((e) => e.id === id);
-  if (!ld) return res.status(404).json({ message: "LD no existe" });
-  res.json(ld);
+
+app.use(cors()); // se usa cuando no estas usadon el sever o 2 maquinas
+app.use(express.json()); // le estas diciendo que estas aciendo en Api no en paginas web o otras cosas
+
+
+app.get("/Id",(req, res)=>{
+  res.json(LDs);
+
+});
+app.get("/Id/:id",(req, res)=>{// id = string
+  const id = Number(req.params.id);// para sacar id que ha introducido
+const eqpenc = LDs.find((elem)=>{
+  if(elem.id===id){
+    res.status(201).json({message:"LD encontrado"})
+    res.json(elem);
+  }
+  else{
+    res.status(404).json({message:"LD no existe"})
+  }
+})
+
+});
+
+app.post("/Id", (req, res) => {
+  try {
+    const error = validateLD(req.body);
+    if (error) return res.status(400).json({ error });
+
+    const newUser: LD = {
+      id: Date.now().toString(),
+      ...req.body,
+    };
+
+    LDs.push(newUser);
+    res.status(201).json(newUser);
+  } catch (err: any) {
+    res
+      .status(500)
+      .json({ error: "Error al crear la persona", detail: err.message });
+  }
+});
+
+app.put("/Id/:id", (req, res) => {
+  try {
+    const  id  = Number(req.params);
+    const index = LDs.findIndex((p) => p.id === id);
+
+    if (index === -1)
+      return res.status(404).json({ error: "Persona no encontrada" });
+
+    const error = validateLD(req.body);
+    if (error) return res.status(400).json({ error });
+
+    LDs[index] = { ...LDs[index], ...req.body };
+  
+    res.json({
+      message: "Persona actualizada correctamente",
+      LD: LDs[index],
+    });
+  } catch (err: any) {
+    res
+      .status(500)
+      .json({ error: "Error al actualizar la persona", detail: err.message });
+  }
+});
+
+app.delete("/Id/:id", (req, res) => {
+  try {
+    const  id  = Number(req.params);
+    const exists = LDs.some((p) => p.id === id);
+
+    if (!exists)
+      return res.status(404).json({ error: "LD no encontrada" });
+
+    LDs = LDs.filter((p) => p.id !== id);
+
+    res.json({ message: "LD eliminada correctamente" });
+  } catch (err: any) {
+    res
+      .status(500)
+      .json({ error: "Error al eliminar LD", detail: err.message });
+  }
 });
 
 
-app.post("/ld", (req, res) => {
-  const error = validateLD(req.body);
-  if (error) return res.status(400).json({ error });
-
-  const newLD: LD = { id: Date.now(), ...req.body };
-  LDs.push(newLD);
-  res.status(201).json(newLD);
-});
+app.listen(port,()=>{
+  console.log("Servidor en http://localhost:"+port )
+})
 
 
-app.put("/ld/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const index = LDs.findIndex((p) => p.id === id);
-  if (index === -1) return res.status(404).json({ error: "LD no encontrada" });
-
-  const error = validateLD(req.body);
-  if (error) return res.status(400).json({ error });
-
-  LDs[index] = { ...LDs[index], ...req.body };
-  res.json({ message: "LD actualizada correctamente", LD: LDs[index] });
-});
-
-
-app.delete("/ld/:id", (req, res) => {
-  const id = Number(req.params.id);
-  LDs = LDs.filter((p) => p.id !== id);
-  res.json({ message: "LD eliminada correctamente" });
-});
-
-
-app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
 
 
 
